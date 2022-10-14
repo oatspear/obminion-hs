2cb1807c47aa8548f1737210bac5e41d637d9ea1
oatspear <oatspear@gmail.com>
2022-10-14 09:52:09 +0100

    labels

--- scenes/ui/labels/icon_value/Label.gd
+++ scenes/ui/labels/icon_value/Label.gd
@@ -1,16 +1,20 @@
+tool
 extends HBoxContainer
 
 ################################################################################
 # Constants
 ################################################################################
 
-const STR_RESOURCE_FORMAT = "%d"
+const STR_VALUE = "%d"
+const STR_USAGE = "%d/%d"
 
 ################################################################################
 # Variables
 ################################################################################
 
 export (int) var current_value: int = 0 setget set_value
+export (int) var max_value: int = 0 setget set_max_value
+export (bool) var display_max: bool = false setget set_display_max_value
 
 onready var label: Label = $Label
 
@@ -24,9 +28,27 @@ func set_value(value: int):
     render()
 
 
+func set_max_value(value: int):
+    max_value = value
+    render()
+
+
+func set_display_max_value(display: bool):
+    display_max = display
+    render()
+
+
 func render():
     if label != null:
-        label.text = STR_RESOURCE_FORMAT % current_value
+        label.text = get_text()
+    elif Engine.editor_hint:
+        $Label.text = get_text()
+
+
+func get_text() -> String:
+    if display_max:
+        return STR_USAGE % [current_value, max_value]
+    return STR_VALUE % current_value
 
 
 ################################################################################
