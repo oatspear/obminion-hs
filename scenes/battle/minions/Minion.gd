fbf454e672ffde33796699ac49f230783ae9a3a9
oatspear <oatspear@gmail.com>
2022-10-16 10:00:25 +0100

    WIP UI

--- scenes/battle/minions/Minion.gd
+++ scenes/battle/minions/Minion.gd
@@ -17,11 +17,60 @@ signal selected()
 # Variables
 ################################################################################
 
+var selectable: bool = false setget set_selectable
+
+onready var sprite: TextureRect = $Elements/Sprite
+onready var label_stats = $Elements/Stats
 onready var animation = $Animation
 
 ################################################################################
 # Interface
 ################################################################################
+
+
+func get_power() -> int:
+    return label_stats.stat1
+
+
+func set_power(value: int):
+    label_stats.stat1 = value
+
+
+func get_health() -> int:
+    return label_stats.stat2
+
+
+func set_health(value: int):
+    label_stats.stat2 = value
+
+
+func set_selectable(is_selectable: bool):
+    if selectable != is_selectable:
+        selectable = is_selectable
+        if selectable:
+            _set_select_style()
+        else:
+            _set_normal_style()
+
+
+################################################################################
+# Helper Functions
+################################################################################
+
+
+func _set_normal_style():
+    sprite.visible = true
+    label_stats.visible = true
+    set("custom_styles/panel", STYLE_NORMAL)
+    if animation.is_playing():
+        animation.stop()
+
+
+func _set_select_style():
+    sprite.visible = false
+    label_stats.visible = false
+    set("custom_styles/panel", STYLE_SELECT)
+    animation.play("pulse")
 
 
 ################################################################################
@@ -32,5 +81,5 @@ onready var animation = $Animation
 func _on_gui_input(event: InputEvent):
     if event is InputEventMouseButton:
         if event.pressed and event.button_index == BUTTON_LEFT:
-            #disable_slot_selectors()
+            set_selectable(not selectable)
             emit_signal("selected")
