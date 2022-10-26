f204ed51f3e1a26a61e40993213996de902ca145
oatspear <oatspear@gmail.com>
2022-10-26 10:44:47 +0100

    minor change

--- scenes/battle/ui/main/PlayerCommandPanel.gd
+++ scenes/battle/ui/main/PlayerCommandPanel.gd
@@ -48,7 +48,7 @@ func show_minion_action_bar(minion_data: Dictionary):
     for action_bar in get_children():
         action_bar.hide()
     # minion_action_bar.reset_ui()
-    $MinionActionBar/InfoCard.set_display_data(minion_data)
+    minion_action_bar.set_minion_data(minion_data)
     minion_action_bar.show()
 
 
