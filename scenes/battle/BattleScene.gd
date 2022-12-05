f28faa3686c6a18cb6aa216eb712de79b7438de9
oatspear <oatspear@gmail.com>
2022-12-05 15:15:45 +0000

    moved code

--- scenes/battle/BattleScene.gd
+++ scenes/battle/BattleScene.gd
@@ -4,4 +4,29 @@ extends Node
 # Variables
 ################################################################################
 
+onready var server: BattleMechanics = BattleMechanics.new()
 onready var gui = $BattleUI
+
+################################################################################
+# Helper Functions
+################################################################################
+
+
+func _default_battle_setup():
+    var p = BattlePlayer.new()
+    p.name = "Player 1"
+    p.max_resources = 5
+    server.players.append(p)
+    p = BattlePlayer.new()
+    p.name = "Player 2"
+    p.max_resources = 5
+    server.players.append(p)
+
+
+################################################################################
+# Event Handlers
+################################################################################
+
+
+func _ready():
+    _default_battle_setup()
