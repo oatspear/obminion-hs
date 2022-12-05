562d931fa6203c6aef47c25dc6bce86828814862
oatspear <oatspear@gmail.com>
2022-12-05 15:12:11 +0000

    Update BattleMechanics.gd

--- scripts/BattleMechanics.gd
+++ scripts/BattleMechanics.gd
@@ -1,6 +1,5 @@
 extends Reference
-
-const BPlayer = preload("res://scripts/BattlePlayer.gd")
+class_name BattleMechanics
 
 ###############################################################################
 # Internal State
@@ -14,13 +13,7 @@ var current_turn: int = 0
 # Interface
 ###############################################################################
 
-func set_players(p1, p2):
-    players = [_battle_player_from_data(p1), _battle_player_from_data(p2)]
-
 
 ###############################################################################
 # Helper Functions
 ###############################################################################
-
-func _battle_player_from_data(player_data):
-    return BPlayer.new()
