f117d80963e7ad3d5c03db79674861d777c95c3b
oatspear <oatspear@gmail.com>
2022-12-05 15:12:03 +0000

    Update BattlePlayer.gd

--- scripts/BattlePlayer.gd
+++ scripts/BattlePlayer.gd
@@ -1,1 +1,26 @@
 extends Reference
+class_name BattlePlayer
+
+################################################################################
+# Variables
+################################################################################
+
+var name: String = "Player"
+
+var resources: int = 0
+var max_resources: int = 1
+
+var minion_deck: Array = []
+var support_deck: Array = []
+
+var active_minions: Array = []
+
+var commander: BattleCommander
+
+################################################################################
+# Interface
+################################################################################
+
+
+func replenish_resources():
+    resources = max_resources
