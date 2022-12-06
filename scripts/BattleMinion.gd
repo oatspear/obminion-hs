353563f304997ce8619fbd416d4bea0cd33cd10f
oatspear <oatspear@gmail.com>
2022-12-06 14:55:41 +0000

    Update BattleMinion.gd

--- scripts/BattleMinion.gd
+++ scripts/BattleMinion.gd
@@ -6,17 +6,45 @@ class_name BattleMinion
 # Variables
 ################################################################################
 
-var name: String = "Minion"
-var faction: String = "Faction"
+var base_data: MinionData setget set_base_data
+
 var tribe: String = "Tribe"
-var species: int = 0
 
 var power: int = 0
 var health: int = 0
+var max_health: int = 0
 var supply: int = 0
 
 var ability: int = 0
 
+var effects: Array = []
+
 ################################################################################
 # Interface
 ################################################################################
+
+
+func set_base_data(data: MinionData):
+    base_data = data
+    tribe = data.tribe
+    power = data.power
+    max_health = data.health
+    health = data.health
+    supply = data.supply
+    ability = data.ability
+
+
+func as_dict() -> Dictionary:
+    return {
+        "name": base_data.name,
+        "type": Global.CardType.MINION,
+        "faction": base_data.faction,
+        "tribe": tribe,
+        "species": base_data.species,
+        "power": power,
+        "health": health,
+        "cost": supply,
+        "resource": Global.ResourceType.RESOURCES,
+        "effect": ability,
+        "effect_text": base_data.ability_text,
+    }
