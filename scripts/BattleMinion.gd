9d26ea2423a27d2e8135c426617169991c2741e1
oatspear <oatspear@gmail.com>
2022-12-26 11:40:57 +0000

    Update BattleMinion.gd

--- scripts/BattleMinion.gd
+++ scripts/BattleMinion.gd
@@ -8,44 +8,50 @@ class_name BattleMinion
 
 # species, instance and base stats
 var instance: MinionInstance setget set_minion_instance
-var base_data: MinionData setget set_base_data
 
 # indices and metadata
 var index: int = -1
 var player_index: int = -1
-var active: bool = false
-var can_act: bool = false
+var action_timer: int = 0
 
-# current/modified stats
-var name: String = "Minion"
-var tribe: String = "Tribe"
-var power: int = 0
-var health: int = 0
-var max_health: int = 0
-var supply: int = 0
-var ability: int = 0
-
-var effects: Array = []
+# battle-specific variables
+var damage: int = 0  # taken
 
 ################################################################################
 # Interface
 ################################################################################
 
 
+func get_name() -> String:
+    return instance.name
+
+
+func get_tribe() -> String:
+    return instance.tribe
+
+
+func get_supply() -> int:
+    return instance.supply
+
+
+func get_power() -> int:
+    return instance.power
+
+
+func get_health() -> int:
+    return instance.health
+
+
+func get_current_health() -> int:
+    return instance.health - damage
+
+
+func get_ability() -> int:
+    return instance.ability
+
+
 func set_minion_instance(minion_instance: MinionInstance):
     instance = minion_instance
-    set_base_data(instance.base_data)
-
-
-func set_base_data(data: MinionData):
-    base_data = data
-    name = data.name
-    tribe = data.tribe
-    power = data.power
-    max_health = data.health
-    health = data.health
-    supply = data.supply
-    ability = data.ability
 
 
 func is_deck_minion() -> bool:
@@ -56,17 +62,22 @@ func is_minion_token() -> bool:
     return instance.is_minion_token()
 
 
+################################################################################
+# Data Transformation Interface
+################################################################################
+
+
 func as_dict() -> Dictionary:
     return {
-        "name": name,
+        "name": get_name(),
         "type": Global.CardType.MINION,
-        "faction": base_data.faction,
-        "tribe": tribe,
-        "species": base_data.species,
-        "power": power,
-        "health": health,
-        "cost": supply,
+        "faction": instance.base_data.faction,
+        "tribe": get_tribe(),
+        "species": instance.species,
+        "power": get_power(),
+        "health": get_health(),
+        "cost": get_supply(),
         "resource": Global.ResourceType.RESOURCES,
-        "effect": ability,
-        "effect_text": base_data.ability_text,
+        "effect": get_ability(),
+        "effect_text": instance.base_data.ability_text,
     }
