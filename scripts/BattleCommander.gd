aa45ea6fe204ec87574ce892257a0b23041c010c
oatspear <oatspear@gmail.com>
2023-01-04 14:31:46 +0000

    Update BattleCommander.gd

--- scripts/BattleCommander.gd
+++ scripts/BattleCommander.gd
@@ -1,2 +1,36 @@
 extends Reference
 class_name BattleCommander
+
+################################################################################
+# Variables
+################################################################################
+
+# metadata
+var player_index: int = -1
+
+# stats
+var power: int = 0
+var health: int = 0
+
+# abilities
+var ability: int = 0
+var effects: Array = []
+
+# battle-specific variables
+var damage: int = 0  # taken
+var divine_shield: bool = false
+
+################################################################################
+# Data Transformation Interface
+################################################################################
+
+
+func as_dict() -> Dictionary:
+    return {
+        "name": "Commander",
+        "type": Global.CardType.COMMANDER,
+        "faction": "FIXME",
+        "power": power,
+        "health": health,
+        "damage": damage,
+    }
